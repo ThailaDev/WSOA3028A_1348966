7113633ffa6aa2612fecc55434ee205204d069a6
ThailaDev <60913394+ThailaDev@users.noreply.github.com>
2020-06-04 12:05:47 +0200

    Added java, style and fixed index.html

---
+++ Js/menu.js
@@ -0,0 +1,9 @@
+const NavSlider = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+
+    burger.addEventListener('click',() => {
+        nav.classList.toggle('.nav-active');
+    });
+}
+NavSlider();
