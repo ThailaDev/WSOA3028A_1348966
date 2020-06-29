e24f6f68373c20312a36c50fda34f059618b07c9
ThailaDev <60913394+ThailaDev@users.noreply.github.com>
2020-06-29 12:57:44 +0200

    additions

--- Js/api.js
+++ Js/api.js
@@ -1,14 +1,7 @@
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=New%20York",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "devru-latitude-longitude-find-v1.p.rapidapi.com",
-		"x-rapidapi-key": "ef0532a7cbmsh74675c67cdd0441p14fce3jsn836ce3d6111d"
-	}
-}
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
+fetch("https://api.chucknorris.io/jokes/random")
+.then((r) => r.json())
+.then((Joke) => {
+	console.log(Joke);
+	JokeHandler(Joke)
 });
+const JokeHandler = (JokeDetails) => {document.querySelector("p.joke").innerText = JokeDetails.value};
